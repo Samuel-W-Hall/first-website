f31abd8f65ee7bd7577010bf19f195798a4bfe02
Samuel-W-Hall <samuelhall2109@gmail.com>
2023-01-26 14:25:42 +0000

    minor changes

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-"use strict";
-console.log("test");
+const slideshow = document.querySelector(".slideshow");
+// const fs = require("fs");
+// fs.readdir("./Best of the Best/", function (err, files) {
+//   //handling error
+//   if (err) {
+//     return console.log("Unable to scan directory");
+//   }
+//   files.forEach(function (file) {
+//     console.log(file);
+//   });
+// });
+
+slideshow.addEventListener("click", function () {
+  // slideshow.src = `./Best of the Best/${}`;
+});
